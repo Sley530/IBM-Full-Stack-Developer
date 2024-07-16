225fb915e09a6b930b7e6ab14ac9c8445fcd7d4e
sley530 <paul-wesleyjeanty@my.uopeople.edu>
2024-07-16 04:50:54 -0600

    need to be improved

--- Unit Conversions/script.js
+++ Unit Conversions/script.js
@@ -2,9 +2,24 @@ function temperature(){
     //To convert celcius to farenheit
     //(CEL * 9/5) + 32
     var c = document.getElementById("celsius").value;
-    var f = (c * 9/5) + 32
-    document.getElementById("fahrenheit").value = f
+    var f = (c * 9/5) + 32;
+    if (c === "") {
+        document.getElementById("fahrenheit").value = "";
+    } else {
+        document.getElementById("fahrenheit").value = f;
+    }
 }
+
+document.getElementById("celsius").addEventListener("input", temperature);
+document.getElementById("fahrenheit").addEventListener("input", function(){
+    var f = document.getElementById("fahrenheit").value;
+    var c = (f - 32) * 5/9;
+    if (f === "") {
+        document.getElementById("celsius").value = "";
+    } else {
+        document.getElementById("celsius").value = c;
+    }
+});
 
 function weight(){
     //To convert kgs to pounds
